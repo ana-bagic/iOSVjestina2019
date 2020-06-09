87e8e55e2bfa7bed1b2e9fdc2b69eeb08651c676
Ana Bagić <ana.bagic13@gmail.com>
2020-06-09 16:31:23 +0200

    2. dz

--- QuizAppFinal/AppDelegate.swift
+++ QuizAppFinal/AppDelegate.swift
@@ -14,11 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        
         window = UIWindow(frame: UIScreen.main.bounds)
-        let vc = InitialViewController()
-        window?.rootViewController = vc
+        window?.backgroundColor = UIColor.white
+        
+        let viewController = InitialViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
         
         return true
